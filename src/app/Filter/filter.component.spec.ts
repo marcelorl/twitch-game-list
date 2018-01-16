41c4e3003027bad3649d9eb27cf083ec2030c783
marcelorl <marcelo_rl2@hotmail.com>
2018-01-16 03:50:07 -0200

    feat: Filter component
    component
    tests

---
+++ src/app/Filter/filter.component.spec.ts
@@ -0,0 +1,84 @@
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import { FilterComponent } from './filter.component';
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+describe('#Component: Filter', () => {
+  let component: FilterComponent;
+  let fixture: ComponentFixture<FilterComponent>;
+  let btnPopularityEl: DebugElement;
+  let btnViewEl: DebugElement;
+
+  const inputGames = [
+    { id: '', name: '', image: '', popularity: 1, viewers: 1 },
+    { id: '', name: '', image: '', popularity: 2, viewers: 2 },
+    { id: '', name: '', image: '', popularity: 3, viewers: 3 }
+  ];
+
+  const expectedGames = [
+    { id: '', name: '', image: '', popularity: 3, viewers: 3 },
+    { id: '', name: '', image: '', popularity: 2, viewers: 2 },
+    { id: '', name: '', image: '', popularity: 1, viewers: 1 }
+  ];
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FilterComponent]
+    });
+
+    fixture = TestBed.createComponent(FilterComponent);
+
+    component = fixture.componentInstance;
+
+    btnPopularityEl = fixture.debugElement.query(By.css('#btn-popularity'));
+    btnViewEl = fixture.debugElement.query(By.css('#btn-views'));
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('checks if class active is in popularity button', () => {
+    btnPopularityEl.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const btnPopClasses = Array.from(btnPopularityEl.nativeElement.classList);
+    const btnViewClasses = Array.from(btnViewEl.nativeElement.classList);
+
+    expect(btnPopClasses.includes('active')).toBeTruthy();
+    expect(btnViewClasses.includes('active')).toBeFalsy();
+  });
+
+  it('checks if class active is in views button', () => {
+    btnViewEl.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const btnViewClasses = Array.from(btnViewEl.nativeElement.classList);
+    const btnPopClasses = Array.from(btnPopularityEl.nativeElement.classList);
+
+    expect(btnViewClasses.includes('active')).toBeTruthy();
+    expect(btnPopClasses.includes('active')).toBeFalsy();
+  });
+
+  it('sorts by popularity', () => {
+    let gamesSorted;
+    component.games = inputGames;
+    component.gamesOutput.subscribe((value) => gamesSorted = value);
+
+    btnPopularityEl.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(gamesSorted).toEqual(expectedGames);
+  });
+
+  it('sorts by views', () => {
+    let gamesSorted;
+    component.games = inputGames;
+    component.gamesOutput.subscribe((value) => gamesSorted = value);
+
+    btnViewEl.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(gamesSorted).toEqual(expectedGames);
+  });
+});
---
+++ src/app/Filter/filter.component.ts
@@ -0,0 +1,61 @@
+import {
+  Component,
+  OnInit,
+  Output,
+  Input,
+  EventEmitter
+} from '@angular/core';
+
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switch';
+
+import { Game } from '../game.model';
+
+@Component({
+  selector: 'app-filter',
+  template: `
+  <div class="d-flex align-items-center pr-3 mt-2">
+    <div class="font-weight-bold mr-2">Filtrar por:</div>
+    <div>
+      <button
+        type="button"
+        id="btn-popularity"
+        class="btn btn-outline-purple"
+        [ngClass]="{'active': sortKey === 'popularity' }"
+        (click)="sortGames('popularity')">
+          Popularity
+        </button>
+      <button
+        type="button"
+        id="btn-views"
+        class="btn btn-outline-purple"
+        [ngClass]="{'active': sortKey === 'views' }"
+        (click)="sortGames('views')">
+          Views
+        </button>
+    </div>
+  </div>
+`
+})
+export class FilterComponent implements OnInit {
+  @Output() gamesOutput: EventEmitter<Game[]> = new EventEmitter<Game[]>();
+  @Input() games: Game[];
+
+  private sortKey: string = 'popularity';
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  sortGames(sortKey: string): void {
+    this.sortKey = sortKey;
+
+    this.gamesOutput.emit(
+      this.games.sort((comp1, comp2) => comp2[sortKey] - comp1[sortKey])
+    );
+  }
+}
